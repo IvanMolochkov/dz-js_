7443e7066e5f923862b9185bf635be7674a114ea
Ivan Molochkov <ivanmolochkov@MacBook-Air-Ivan.local>
2024-03-14 17:33:05 +0300

    Исправление десятого ДЗ

--- 10-array-clean/array-clean.js
+++ 10-array-clean/array-clean.js
@@ -1,31 +1,33 @@
-const arrayOfNumbers = [9,8,7,6,5,4,3,2,1];
+let arrayOfNumbers = [9, 8, 7, 6, 5, 4, 3, 2, 1];
 
-const remove = (arr, number) => {
-    let newArr = [];
-    for (const el of arr) {
-        if (el === number) {
-            continue;
-        }
-        newArr.push(el);
-    };
-    return newArr
+const remove = (arr, num) => {
+  let newArray = [];
+  let change = false;
+  for (const el of arr) {
+    if (num === el) {
+      change = true;
+      continue;
+    }
+    newArray.push(el);
+  }
+  return [change, newArray];
 };
 
 const selection = (arr, number, func) => {
-    const array = func(arr, number);
-    array.length < arr.length ? console.log(true) : console.log(false);
-    for (let i = 0; i < array.length; i++) {
-      let indexMin = i;
-      for (let j = i + 1; j < array.length; j++) {
-        if (array[j] < array[indexMin]) {
-          indexMin = j;
-        }
+  let [bool, array] = func(arr, number);
+  console.log(bool);
+  for (let i = 0; i < array.length; i++) {
+    let indexMin = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[indexMin]) {
+        indexMin = j;
       }
-      let tmp = array[i];
-      array[i] = array[indexMin];
-      array[indexMin] = tmp;
     }
-    return array;
-  };
+    let tmp = array[i];
+    array[i] = array[indexMin];
+    array[indexMin] = tmp;
+  }
+  return array;
+};
 
 console.log(selection(arrayOfNumbers, 5, remove));
